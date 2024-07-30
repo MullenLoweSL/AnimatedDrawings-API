7abe18ba79984053e0fe3153bf4b5c2b34a5614d
tech guru <techguru2307@gmail.com>
2024-07-30 15:02:05 +0530

    feat: Add POST API to generate MP4 file from drawing image in Flask app.

--- examples/annotations_to_animation.py
+++ examples/annotations_to_animation.py
@@ -39,6 +39,50 @@ def annotations_to_animation(char_anno_dir: str, motion_cfg_fn: str, retarget_cf
     # render the video
     animated_drawings.render.start(output_mvc_cfn_fn)
 
+def custom_annotations_to_animation(char_anno_dir: str, motion_cfg_fn: str, retarget_cfg_fn: str, bg_image: str = None):
+    """
+    Given a path to a directory with character annotations, a motion configuration file, and a retarget configuration file,
+    creates an animation and saves it to {annotation_dir}/video.png
+    """
+
+    # package character_cfg_fn, motion_cfg_fn, and retarget_cfg_fn
+    animated_drawing_dict = {
+        'character_cfg': str(Path(char_anno_dir, 'char_cfg.yaml').resolve()),
+        'motion_cfg': str(Path(motion_cfg_fn).resolve()),
+        'retarget_cfg': str(Path(retarget_cfg_fn).resolve())
+    }
+
+    # create mvc config
+    mvc_cfg = {
+        'scene': {'ANIMATED_CHARACTERS': [animated_drawing_dict]},  # add the character to the scene
+        'controller': {
+            'MODE': 'video_render',  # 'video_render' or 'interactive'
+            'OUTPUT_VIDEO_PATH': str(Path(char_anno_dir, 'video.mp4').resolve())
+        }  # set the output location
+    }
+
+    view_dict = {}
+    if bg_image:
+        view_dict = {
+            "CAMERA_POS": [0.1, 1.3, 2.7],
+            "USE_MESA": True,
+            "WINDOW_DIMENSIONS": [300, 400],
+            "BACKGROUND_IMAGE": f"uploads/bg_images/{bg_image}",
+        }
+        mvc_cfg['view'] = view_dict
+
+    mvc_cfg['controller']['OUTPUT_VIDEO_CODEC'] = 'mp4v'
+    mvc_cfg['view'] = view_dict
+    mvc_cfg['view']["USE_MESA"] = False
+
+    # write the new mvc config file out
+    output_mvc_cfn_fn = str(Path(char_anno_dir, 'mvc_cfg.yaml'))
+    with open(output_mvc_cfn_fn, 'w') as f:
+        yaml.dump(dict(mvc_cfg), f)
+
+    # render the video
+    animated_drawings.render.start(output_mvc_cfn_fn)
+
 
 if __name__ == '__main__':
 
